4e03ead83a52b22613d1816ed0e87218d5de7125
Raquelleal3 <raquelleal957@gmail.com>
2024-10-16 16:25:31 +0100

    Add files via upload

---
+++ bdcidade.sql
@@ -0,0 +1,17 @@
+	insert into Cidades(nome, codigo_postal) values
+    ('Lisboa','1000'),
+    ('Porto','4150'),
+    ('Vila Nova de Gaia','4430'),
+    ('Amadora','1500'),
+    ('Braga', '4700'),
+    ('Funchal','9000'),
+    ('Coimbra','3030'),
+    ('Setúbal','2900'),
+    ('Almada','2800'),
+    ('Alguva-Cacém','2735'),
+    ('Queluz','4800'),
+    ('Rio Tinto','4436'),
+    ('Barreio','2830'),
+    ('Leiria','2400');
+    
+    select * from Cidades;
